a9c4c63adc63736f979ae9c7968960f3676d3c43
Terry Keeney <terry.keeney@razorfish.com>
2017-01-12 10:55:12 -0700

    goto torchys order page

--- popup.js
+++ popup.js
@@ -4,13 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('Things happening')
 });
 
+document.querySelector('#goto').addEventListener('click', function(e) {
+    chrome.tabs.create({ url: torchysOrderPage });
+    window.close();
+})
+
 
 chrome.tabs.query({ active: true }, function(tabs) {
     const currentTab = tabs[0];
     const pageUrl = currentTab.url;
-    console.log(pageUrl)
+    const parts = pageUrl.split('/');
 
-    if (pageUrl.startsWith(torchysOrderPage)) {
+
+    if (pageUrl.startsWith(torchysOrderPage) && parts[4]) {
         document.querySelector('.menu').classList.toggle('hide-it');
         document.querySelector('.navigation').classList.toggle('hide-it');
     }
