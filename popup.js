81724ab60d51a8b9a5e494260ddf375de9e60563
Terry Keeney <terry.keeney@razorfish.com>
2017-01-12 09:52:26 -0700

    Add section for going to order page and toggle betweeen menu and navigation depending on page

--- popup.js
+++ popup.js
@@ -1,7 +1,17 @@
+const torchysOrderPage = 'https://order.torchystacos.com/torchys';
 
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('Things happening')
-  chrome.tabs.getSelected(null,function(t){
-    document.querySelector('ul').style  
-  });
+    console.log('Things happening')
 });
+
+
+chrome.tabs.query({ active: true }, function(tabs) {
+    const currentTab = tabs[0];
+    const pageUrl = currentTab.url;
+    console.log(pageUrl)
+
+    if (pageUrl.startsWith(torchysOrderPage)) {
+        document.querySelector('.menu').classList.toggle('hide-it');
+        document.querySelector('.navigation').classList.toggle('hide-it');
+    }
+})
